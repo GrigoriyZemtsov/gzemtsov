c2616a56ea7cf6f1f8563bd6f1871e21e549ac88
GrigoriyZemtsov <griffondark@gmail.com>
2018-02-20 11:10:47 +0300

    2.2 Import Maven

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -13,7 +13,7 @@ public class Calculate {
 	 * @param String строка
 	 * @return вывод сообщения
 	 */
-	public static void main(String[] args) {
+	 public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
 	
