6f6bd3dcc052c02c5c86c7da28eb02950bde5eec
GrigoriyZemtsov <griffondark@gmail.com>
2018-04-04 11:06:30 +0300

    4.2 Максимум из двух чисел.

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,16 @@
+package ru.job4j.max;
+
+/**
+ * Программа возвращает максимальное значение из двух заданных чисел
+ * @autor gzemtsov
+ * @versoin $Id$
+ * @since 0.1
+ */
+ public class Max {
+	 public int max(int first, int second) {
+		 /**
+		  * Метод возвращает максимальное значений из двух заданных чисел
+		  */
+		  return (first > second ? first : second);
+	 }
+ }
