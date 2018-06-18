db47d8fb95c8874024442070382061a08810da68
GrigoriyZemtsov <griffondark@gmail.com>
2018-06-18 11:44:29 +0300

    5.5 Рефакторинг кода

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -10,14 +10,7 @@ import java.util.function.BiPredicate;
  */ 
  
  public class Paint {
-	 public String rightTrl(int height) {
-		 return this.loopBy(height, height, (row, column) -> row >= column);
-	 }
-	 
-	 public String leftTrl(int height) {
-		 return this.loopBy(height, height, (row, column) -> row >= height - column - 1);
-	 }
-	 
+
 	 public String pyramid(int height) {
 		return this.loopBy(height, 2 * height - 1, (row, column) -> row >= height - column - 1 && row + height - 1 >= column);
 	}
