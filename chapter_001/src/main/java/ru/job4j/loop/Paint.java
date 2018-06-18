df7b552ca9ff70b7bb88bfbafc5eee39c1e438fe
GrigoriyZemtsov <griffondark@gmail.com>
2018-06-18 11:28:31 +0300

    5.5 Рефакторинг кода

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,18 +1,32 @@
 package ru.job4j.loop;
+
+import java.util.function.BiPredicate;
+
 /**
  * Программа построения пирамиды в псевдографике.
  * @autor Grigory Zemtsov
  * @since 10/06/2018
  * @version 0.01
- */
+ */ 
  
  public class Paint {
+	 public String rightTrl(int height) {
+		 return this.loopBy(height, height, (row, column) -> row >= column);
+	 }
+	 
+	 public String leftTrl(int height) {
+		 return this.loopBy(height, height, (row, column) -> row >= height - column - 1);
+	 }
+	 
 	 public String pyramid(int height) {
+		return this.loopBy(height, 2 * height - 1, (row, column) -> row >= height - column - 1 && row + height - 1 >= column);
+	}
+	
+	private String loopBy(int height, int weight, BiPredicate<Integer, Integer> predict) {
 		StringBuilder screen = new StringBuilder();
-		int weight = 2 * height - 1;
 		for (int row = 0; row != height; row++) {
 			for (int column = 0; column != weight; column++) {
-				if (row >= height - column - 1 && row + height - 1 >= column) {
+				if (predict.test(row, column)) {
 					screen.append("^");
 				} else {
 					screen.append(" ");
@@ -20,6 +34,6 @@ package ru.job4j.loop;
 			}
 			screen.append(System.lineSeparator());
 		}
-    return screen.toString();
+		return screen.toString();
 	}
  }
