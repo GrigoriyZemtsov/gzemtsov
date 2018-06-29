8ead7594533dc745b21f3e29268ea1744721975c
GrigoriyZemtsov <griffondark@gmail.com>
2018-06-29 12:58:06 +0300

    6.1 Классический поиск перебором

---
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+/**
+ * Программа поиска элемента в массиве, методом перебора.
+ * @autor Grigory Zemtsov
+ * @version 001
+ */
+ 
+ public class FindLoop {
+	 public int indexOf(int[] data, int el) {
+		 int rst = -1; // если элемента нет в массиве, то возвращается значение -1.
+		 for (int index = 0; index < data.length; index++) {
+			 if (data[index] == el) {
+				 rst = index;
+				 break;
+			 } else {
+			 	rst = -1;
+			 }
+		 }
+		 return rst;
+	 }
+ }
--- chapter_001/src/main/java/ru/job4j/array/Square.java
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -4,11 +4,9 @@ package ru.job4j.array;
 public class Square {
 	public int[] calculate(int bound) {
 		int[] rst = new int[bound];
-		int i = 1;
 		//заполнить массив через цикл от 1 до bound.
-		for (int index = 0; index != rst.length; index++) {
-			rst[index] = i * i;
-			i++;
+		for (int index = 1; index <= rst.length; index++) {
+			rst[index - 1] = index * index;
 		}
 		return rst;
 	}
