47a71323d8b42dfc6c203e2e7e4c524c2ca4fbf2
GrigoriyZemtsov <griffondark@gmail.com>
2018-07-22 22:01:58 +0300

    6.4 Массив заполнен true или false

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -13,16 +13,14 @@ package ru.job4j.array;
 		 this.data = line.toCharArray();
 	 }
 	 /**
-	  * Метод проверки префикса с которого начинаются слова
+	  * Метод сравнения префикса с началом слова
 	  * @param prefix - префикс.
-	  * @return, если слово начинается с префикса.
+	  * @return boolean.
 	  */
 	  public boolean startWith(String prefix) {
 		  boolean result = true;
 		  char[] value = prefix.toCharArray();
-		  // осуществляем проверку, пока не достигнем конца наиболее короткой строки.
 		  int minLength = Math.min(value.length, data.length);
-		  // проверить массив data на одинаковость элементов с value.
 		  for (int i = 0; i < minLength; i++) {				
 				if (value[i] != data[i]) {
 					result = false;
