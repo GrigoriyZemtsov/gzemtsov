9c3a9086a3abf85717f0638babdd507b18a22490
GrigoriyZemtsov <griffondark@gmail.com>
2018-08-14 21:00:17 +0300

    6.5 Таблица умножения

---
+++ chapter_001/src/main/java/ru/job4j/array/Matrix.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+/**
+ * Программа заполняющая таблицу умножения на заданный размер.
+ * @autor Gregory Zemtsov
+ * @version 001 30.07.2018
+ */
+ 
+ public class Matrix {
+	 public int[][] multiple(int size) {
+		 int[][] table = new int[size][size];
+		 for (int i = 0; i < table.length; i++) {
+		 	for (int j = 0; j < table.length; j++) {
+				table [i][j] = (i + 1) * (j + 1);
+		 }
+		 }
+		 return table;
+	 }
+ }
