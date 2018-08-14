c0baadda3620086a30b75db0b397e65caf0b1c51
GrigoriyZemtsov <griffondark@gmail.com>
2018-08-14 21:05:23 +0300

    6.6 Таблица умножения

--- chapter_001/src/main/java/ru/job4j/array/Matrix.java
+++ chapter_001/src/main/java/ru/job4j/array/Matrix.java
@@ -12,7 +12,7 @@ package ru.job4j.array;
 		 for (int i = 0; i < table.length; i++) {
 		 	for (int j = 0; j < table.length; j++) {
 				table [i][j] = (i + 1) * (j + 1);
-		 }
+		    }
 		 }
 		 return table;
 	 }
