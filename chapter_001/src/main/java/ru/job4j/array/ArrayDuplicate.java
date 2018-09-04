8682f5b009a3f75e57a340ddd1af8d3a9da874d7
GrigoriyZemtsov <griffondark@gmail.com>
2018-09-04 12:53:02 +0300

    6.8. Удаление дубликатов в массиве.

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -0,0 +1,30 @@
+package ru.job4j.array;
+
+/**
+ * Программа удаления дубликатов в строковом массиве
+ * @autor Gregory Zemtsov (zemcov.rash@gmail.com).
+ * @version 001 03.09.2018
+ */
+
+ import java.util.Arrays;
+ 
+ public class ArrayDuplicate {
+	 public String[] remove(String[] array) {
+		 int dup = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length - dup; j++) {
+                if (array[i].equals(array[j])) {
+                    while (j != array.length - dup - 1
+                        && array[array.length - dup - 1].equals(array[j])) {
+                        dup++;
+                    }
+                    String tmp = array[array.length - dup - 1];
+                    array[array.length - dup - 1] = array[j];
+                    array[j] = tmp;
+                    dup++;
+                }
+            }
+        }
+    return Arrays.copyOf(array, array.length - dup);
+	 }
+ }
---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.array;
+
+/**
+ * Тест проверки работы класса ArrayDuplicate
+ * @autor Gregory Zemtsov (zemcov.rash@gmail.com)
+ * @version 001 03.09.18
+ */
+ 
+ import org.junit.Test;
+ import static org.hamcrest.core.Is.is;
+ import static org.junit.Assert.assertThat;
+ 
+ public class ArrayDuplicateTest {
+	 @Test
+	 public void whenRemoveDuplicatesThenArrayWithoutDuplicate1() {
+		 String[] bound = {"Привет", "Мир", "Привет", "Супер", "Мир"};
+		 ArrayDuplicate duplicate = new ArrayDuplicate();
+		 String[] resultArray;
+		 resultArray = duplicate.remove(bound);
+		 String[] expect = {"Привет", "Мир", "Супер"};
+		 assertThat(resultArray, is(expect));
+	 }
+
+	 @Test
+	 public void whenRemoveDuplicatesThenArrayWithoutDuplicate2() {
+		 String[] bound = {"Привет", "Мир", "Привет", "Супер", "Мир", "Ура", "Пока", "Ура"};
+		 ArrayDuplicate duplicate = new ArrayDuplicate();
+		 String[] resultArray;
+		 resultArray = duplicate.remove(bound);
+		 String[] expect = {"Привет", "Мир", "Ура", "Супер", "Пока"};
+		 assertThat(resultArray, is(expect));
+	 }
+ }
